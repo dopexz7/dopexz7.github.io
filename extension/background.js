a1fc506fe0793a5908aeea5d3b3a6d789045afa1
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-01-01 19:41:41 +0200

    k

---
+++ extension/background.js
@@ -0,0 +1,22 @@
+(function() {
+    chrome.runtime.onInstalled.addListener(function(details) {
+        if (details.reason == "install") {
+            chrome.storage.local.get(['SET'], function(result) { //check for custom emotes at install
+                if (result.SET == null) { //if no emote set is saved
+                    var emotes = [];
+                    chrome.storage.local.set({ SET: emotes }); //save default set
+                }
+            });  
+        } else if(details.reason == 'update') {
+            chrome.storage.local.get('vers034', function(result){
+                if(!result.vers034) {
+                    chrome.tabs.create({url: "update.html"});
+                    chrome.storage.local.set({ vers034: 'dontdisplaythisshit' });
+                }
+                
+            });
+            
+        }       
+    });
+
+}());
