7f7d2e122093acd3e551bd0dbab0257eb1ebdcf6
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-01-06 01:48:42 +0200

    j

--- nopixel/main.js
+++ nopixel/main.js
@@ -1,25 +1,23 @@
 
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-}
+// function checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//       statusChangeCallback(response);
+//     });
+// }
 function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
+  //console.log('statusChangeCallback');
+  //console.log(response);
   if (response.status === 'connected') {
-      alert('loggedIn')
       testAPI();
+  } else {
+    document.getElementById('user_name').textContent = 'Not connected';
   }
 }
 
-
 function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
+    console.log('Welcome! Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
       document.getElementById('user_name').textContent = response.name;
-      console.log(response)
     });
     location.reload();
 }
