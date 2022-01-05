a790e42e0a000daf2a5db4697cb97ee80298e746
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-01-06 00:07:55 +0200

    d

--- nopixel/main.js
+++ nopixel/main.js
@@ -1,52 +1,31 @@
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    if (response.status === 'connected') {
-      alert('loggedIn')
-      testAPI();
-
-    } else if (response.status === 'not_authorized') {
-      FB.login(function(response) {
-        statusChangeCallback2(response);
-      }, {scope: 'public_profile,email'});
-
-    } else {
-      alert("not connected, not logged into facebook, we don't know");
-    }
-  }
-
-  function statusChangeCallback2(response) {
-    console.log('statusChangeCallback2');
-    console.log(response);
-    if (response.status === 'connected') {
-      testAPI();
-
-    } else if (response.status === 'not_authorized') {
-      console.log('still not authorized!');
-
-    } else {
-      alert("not connected, not logged into facebook, we don't know");
-    }
-  }
-
+setTimeout(function(){
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
+}
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  if (response.status === 'connected') {
+      alert('loggedIn')
+      testAPI();
   }
+}
 
-  function testAPI() {
+
+function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('user_name').textContent = response.name;
     });
-  }
+}
 
 window.fbAsyncInit = function() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 };
 
 
@@ -57,3 +36,15 @@ function checkLoginState() {
   });
 }
 
+FB.api(
+    "/Ramee/live_videos",
+    function (response) {
+      if (response && !response.error) {
+        console.log(response)
+      }
+    }
+);
+
+
+
+},2000);
