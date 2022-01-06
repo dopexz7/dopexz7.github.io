a6353c873691e800a33c8c0bd44d3ad95960d33d
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-01-06 02:13:02 +0200

    Update main.js

--- nopixel/main.js
+++ nopixel/main.js
@@ -8,8 +8,9 @@ function statusChangeCallback(response) {
   //console.log('statusChangeCallback');
   //console.log(response);
   if (response.status === 'connected') {
+      document.getElementById('user_name').textContent = response.name;
       testAPI();
-      console.log(response.name)
+
   } else {
     document.getElementById('user_name').textContent = 'Not connected';
   }
@@ -20,7 +21,7 @@ function testAPI() {
     console.log('Welcome! Fetching your information.... ');
     FB.api('/me', function(response) {
       lmao = response.name;
-      document.getElementById('user_name').textContent = 'response.name';
+      
       console.log(response.name)
 
     });
