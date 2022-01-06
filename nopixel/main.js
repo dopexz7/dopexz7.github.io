b6a3b58484c81997ebb69ab9c92fd3a2d7ecb5e9
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-01-06 02:16:03 +0200

    ddxddx

--- nopixel/main.js
+++ nopixel/main.js
@@ -4,29 +4,6 @@
 //       statusChangeCallback(response);
 //     });
 // }
-function statusChangeCallback(response) {
-  //console.log('statusChangeCallback');
-  //console.log(response);
-  if (response.status === 'connected') {
-      document.getElementById('user_name').textContent = response.name;
-      testAPI();
 
-  } else {
-    document.getElementById('user_name').textContent = 'Not connected';
-  }
-}
-
-function testAPI() {
-    console.log('Welcome! Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log(response.name)
-    });
-}
-
-window.fbAsyncInit = function() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-};
 
 
